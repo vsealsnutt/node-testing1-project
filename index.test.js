eff5667e171a17466a1798840fc5b5ddc08fe738
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-23 12:15:46 -0400

    wrote test 15

--- index.test.js
+++ index.test.js
@@ -104,10 +104,15 @@ describe('[Exercise 6] Car', () => {
   beforeEach(() => {
     focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
   })
-  // test('[15] driving the car returns the updated odometer', () => {})
-  // test('[16] driving the car uses gas', () => {})
-  // test('[17] refueling allows to keep driving', () => {})
-  // test('[18] adding fuel to a full tank has no effect', () => {})
+  test('[15] driving the car returns the updated odometer', () => {
+    expect(focus.drive(100)).toBe(100)
+    expect(focus.drive(100)).toBe(200)
+    expect(focus.drive(100)).toBe(300)
+    expect(focus.drive(200)).toBe(500)
+  })
+  test('[16] driving the car uses gas', () => {})
+  test('[17] refueling allows to keep driving', () => {})
+  test('[18] adding fuel to a full tank has no effect', () => {})
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
