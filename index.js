194717bd578001e93de8c9f36229497e90760001
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-23 12:45:15 -0400

    wrote drive function. tests 15 & 16 passing

--- index.js
+++ index.js
@@ -128,7 +128,8 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
-    this.tankSize = tankSize
+    this.tankSize = tankSize;
+    this.mpg = mpg;
   }
 
   /**
@@ -146,7 +147,14 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
-    this.odometer = this.odometer + distance;
+    const maxMiles = this.tank * this.mpg;
+    if (distance <= maxMiles) {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.mpg)
+    } else {
+      this.odometer = this.odometer + maxMiles;
+      this.tank = 0;
+    }
     return this.odometer;
   }
 
